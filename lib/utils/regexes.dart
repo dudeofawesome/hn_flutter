3b4784dc1f4b47917332892ee2ec92c0bc218ac7
Louis Orleans <louis@orleans.io>
2018-01-06 01:38:58 -0700

    🐛 fix URL matching

--- lib/utils/regexes.dart
+++ lib/utils/regexes.dart
@@ -1,3 +1,3 @@
-final htmlTagA = new RegExp(r'\<a.*?href\=\\?"([a-z0-9\/\-_\.:\&\?\=\#]*)\\?".*?\>(.*?)\<\/a\>', caseSensitive: false);
+final htmlTagA = new RegExp(r'\<a.*?href\=\\?"([a-z0-9\/\-_\.:\&\?\=\#\%]*)\\?".*?\>(.*?)\<\/a\>', caseSensitive: false);
 final fullUri = new RegExp(r'(\S+)\:\/\/([^\/\s]+)([\S]*)', caseSensitive: false);
 final looseUrl = new RegExp(r'((\S+)\:\/\/)?([^\/\s]+?\.[^\/\s]+)([\S]*)', caseSensitive: false);
