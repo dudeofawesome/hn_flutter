06098f5e4c3b7656c6fced0a3637f1258b248a4d
Louis Orleans <louis@orleans.io>
2018-01-07 06:13:00 -0700

    ✅ add htmlTagA test case

--- test/utils/regexes_test.dart
+++ test/utils/regexes_test.dart
@@ -11,6 +11,7 @@ void main () {
         htmlTagA.hasMatch(r'<a href=\"http://blog.airbornos.com/post/2017/08/03/Transparent-Web-Apps-using-Service-Worker\" rel=\"nofollow\">http://blog.airbornos.com/post/2017/08/03/Transparent-Web-Ap...</a>'),
         htmlTagA.hasMatch(r'<a href=\"https://lwn.net/Articles/720336/\" rel=\"nofollow\">https://lwn.net/Articles/720336/</a>'),
         htmlTagA.hasMatch(r'<a href=\"https://lwn.net/Articles/720336?id=42&name=louis" rel="nofollow">Some link text here.</a>'),
+        htmlTagA.hasMatch(r'''<a href=\"https://en.wikipedia.org/wiki/Duff's_device\" rel=\"nofollow\">https://en.wikipedia.org/wiki/Duff's_device</a>'''),
       ];
       expect(answers.every((answer) => answer == true), true);
     });
