dafa2b7cd12ffa2ebcddc4156b6d1bdb39c63f41
Louis Orleans <louis@orleans.io>
2018-01-07 05:48:38 -0700

    🐛✅ add tests and fix but in htmlTagA regex

---
+++ test/utils/regexes_test.dart
@@ -0,0 +1,18 @@
+import 'package:test/test.dart';
+
+import 'package:hn_flutter/utils/regexes.dart';
+
+void main () {
+  group('regexes', () {
+    test('htmlTagA unit test', () {
+      final answers = [
+        htmlTagA.hasMatch(r'<a href="https://posteo.de/en/blog/new-easy-email-encryption-with-autocrypt-and-openpgp-header" rel="nofollow">https://posteo.de/en/blog/new-easy-email-encryption-with-aut...</a>'),
+        htmlTagA.hasMatch(r'<a href=\"https://posteo.de/en/blog/new-easy-email-encryption-with-autocrypt-and-openpgp-header\" rel=\"nofollow\">https://posteo.de/en/blog/new-easy-email-encryption-with-aut...</a>'),
+        htmlTagA.hasMatch(r'<a href=\"http://blog.airbornos.com/post/2017/08/03/Transparent-Web-Apps-using-Service-Worker\" rel=\"nofollow\">http://blog.airbornos.com/post/2017/08/03/Transparent-Web-Ap...</a>'),
+        htmlTagA.hasMatch(r'<a href=\"https://lwn.net/Articles/720336/\" rel=\"nofollow\">https://lwn.net/Articles/720336/</a>'),
+        htmlTagA.hasMatch(r'<a href=\"https://lwn.net/Articles/720336?id=42&name=louis" rel="nofollow">Some link text here.</a>'),
+      ];
+      expect(answers.every((answer) => answer == true), true);
+    });
+  });
+}
