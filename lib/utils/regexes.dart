429f1a49c04f3abdcd649734b4f9088d40b548e1
Louis Orleans <louis@orleans.io>
2018-01-07 06:12:45 -0700

    🐛 looseUrl now requires a > 2 char TLD

--- lib/utils/regexes.dart
+++ lib/utils/regexes.dart
@@ -1,4 +1,4 @@
 final htmlTagA = new RegExp(r'\<a.*?href\=\\?"(' + urlSafeCharacters + r'*)\\?".*?\>(.*?)\<\/a\>', caseSensitive: false);
 final urlSafeCharacters = r"[a-zA-Z0-9\-\.\_\~\:\/\?\#\[\]\@\!\$\&\'\(\)\*\+\,\;\=]";
 final fullUri = new RegExp(r'(\S+)\:\/\/([^\/\s]+)([\S]*)', caseSensitive: false);
-final looseUrl = new RegExp(r'((\S+)\:\/\/)?([^\/\s]+?\.[^\/\s]+)([\S]*)', caseSensitive: false);
+final looseUrl = new RegExp(r'((\S+)\:\/\/)?([^\/\s]+?\.[^\/\s]{2,})([\S]*)', caseSensitive: false);
