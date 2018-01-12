251cb9561ece8741dd8cb1ea2cff3fd3b022ea48
Louis Orleans <louis@orleans.io>
2018-01-12 00:06:35 -0800

    🚧✨🔨 refactoring DB interactions

--- lib/sdk/hn_auth_service.dart
+++ lib/sdk/hn_auth_service.dart
@@ -8,69 +8,19 @@ import 'package:path_provider/path_provider.dart';
 
 import 'package:hn_flutter/sdk/sqlite_vals.dart';
 import 'package:hn_flutter/sdk/hn_config.dart';
+import 'package:hn_flutter/sdk/actions/hn_account_actions.dart';
+import 'package:hn_flutter/sdk/models/hn_account.dart';
 
 class HNAuthService {
   static final HNAuthService _singleton = new HNAuthService._internal();
 
   HNConfig _config = new HNConfig();
 
-  Directory _documentsDirectory;
-  Database _keysDb;
-  Database _accountsDb;
-
-  String _primaryUserId = '';
-  String _primaryUserPassword = '';
-
-  HNAuthService._internal () {
-    new Future(() async {
-      this._documentsDirectory = await getApplicationDocumentsDirectory();
-
-      String keysPath = join(this._documentsDirectory.path, 'keys.db');
-      this._keysDb = await openDatabase(keysPath, version: 1, onCreate: (Database db, int version) async {
-        await db.execute('CREATE TABLE $KEYS_TABLE ($KEYS_ID TEXT PRIMARY KEY, $KEYS_VALUE TEXT)');
-      });
-
-      final primaryUserIdKeys = await this._keysDb.query(
-        KEYS_TABLE,
-        columns: [KEYS_ID, KEYS_VALUE],
-        where: '$KEYS_ID = ?',
-        whereArgs: [KEY_PRIMARY_ACCOUNT_ID],
-        limit: 1,
-      );
-
-      if (primaryUserIdKeys.length == 0) {
-        this._primaryUserId = '';
-      } else {
-        print(primaryUserIdKeys.first);
-        print(primaryUserIdKeys.first[KEYS_VALUE]);
-        // this._primaryUserId = primaryUserIdKeys.first;
-
-        String accountsPath = join(this._documentsDirectory.path, ACCOUNTS_DB);
-        this._accountsDb = await openDatabase(accountsPath, version: 1, onCreate: (Database db, int version) async {
-          await db.execute('CREATE TABLE $ACCOUNTS_TABLE ($ACCOUNTS_ID TEXT PRIMARY KEY, $ACCOUNTS_PASSWORD TEXT, $ACCOUNTS_ACCESS_TOKEN TEXT)');
-        });
-
-        final accounts = await this._keysDb.query(
-          ACCOUNTS_TABLE,
-          columns: [ACCOUNTS_ID, ACCOUNTS_PASSWORD, ACCOUNTS_ACCESS_TOKEN],
-          where: '$ACCOUNTS_ID = ?',
-          whereArgs: [this._primaryUserId],
-        );
-
-        print(accounts);
-
-        final primaryUser = accounts.firstWhere((account) => account[''] == this._primaryUserId);
-        this._primaryUserPassword = primaryUser[ACCOUNTS_PASSWORD];
-      }
-    });
-  }
+  HNAuthService._internal ();
 
   factory HNAuthService () {
     return _singleton;
   }
-
-  get primaryUserId => this._primaryUserId;
-  get primaryUserPassword => this._primaryUserPassword;
 
   Future<bool> addAccount (String userId, String userPassword) async {
     return http.post(
@@ -81,11 +31,42 @@ class HNAuthService {
         },
       )
       .then((res) {
-        if (res.statusCode == 200) {
+        print(res);
+        print(res.body);
+        if (!res.body.contains('Bad login.')) {
+          addHNAccount(new HNAccount(
+            id: userId,
+            password: userPassword,
+          ));
           return true;
         } else {
           return false;
         }
       });
+  }
+
+  Future<bool> removeAccount (String userId) async {
+    // return http.post(
+    //     '${this._config.apiHost}/login',
+    //     body: {
+    //       'acct': userId,
+    //       'pw': userPassword,
+    //     },
+    //   )
+    //   .then((res) {
+    //     print(res);
+    //     print(res.body);
+    //     if (!res.body.contains('Bad login.')) {
+    //       addHNAccount(new HNAccount(
+    //         id: userId,
+    //         password: userPassword,
+    //       ));
+    //       return true;
+    //     } else {
+    //       return false;
+    //     }
+    //   });
+    removeHNAccount(userId);
+    return true;
   }
 }
---
+++ lib/sdk/stores/hn_account_store.dart
@@ -0,0 +1,138 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter_flux/flutter_flux.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+
+import 'package:hn_flutter/sdk/actions/hn_account_actions.dart';
+import 'package:hn_flutter/sdk/models/hn_account.dart';
+import 'package:hn_flutter/sdk/sqlite_vals.dart';
+
+class HNAccountStore extends Store {
+  Directory _documentsDirectory;
+  Database _keysDb;
+  Database _accountsDb;
+
+  String _primaryAccountId;
+  final Map<String, HNAccount> _accounts = new Map();
+
+  HNAccountStore () {
+    new Future(() async {
+      this._documentsDirectory = await getApplicationDocumentsDirectory();
+
+      String keysPath = join(this._documentsDirectory.path, KEYS_DB);
+      this._keysDb = await openDatabase(keysPath, version: 1, onCreate: (Database db, int version) async {
+        print('CREATING KEYS TABLE');
+        await db.execute('CREATE TABLE $KEYS_TABLE ($KEYS_ID TEXT PRIMARY KEY, $KEYS_VALUE TEXT)');
+      });
+
+      final primaryUserIdKeys = await this._keysDb.query(
+        KEYS_TABLE,
+        columns: [KEYS_ID, KEYS_VALUE],
+        where: '$KEYS_ID = ?',
+        whereArgs: [KEY_PRIMARY_ACCOUNT_ID],
+        limit: 1,
+      );
+
+      String accountsPath = join(this._documentsDirectory.path, ACCOUNTS_DB);
+      this._accountsDb = await openDatabase(accountsPath, version: 1, onCreate: (Database db, int version) async {
+        print('CREATING ACCOUNTS TABLE');
+        await db.execute('''
+          CREATE TABLE $ACCOUNTS_TABLE
+            ($ACCOUNTS_ID TEXT PRIMARY KEY, $ACCOUNTS_EMAIL TEXT, $ACCOUNTS_PASSWORD TEXT, $ACCOUNTS_ACCESS_TOKEN TEXT)
+        ''');
+      });
+
+      final accounts = await this._accountsDb.query(
+        ACCOUNTS_TABLE,
+        columns: [ACCOUNTS_ID, ACCOUNTS_PASSWORD, ACCOUNTS_ACCESS_TOKEN],
+      );
+
+      print(accounts);
+
+      accounts
+        .map((accountMap) => new HNAccount.fromMap(accountMap))
+        .forEach((account) {
+          print(account);
+          // this._accounts[account.id] = account;
+          // TODO: this causes the DB to get rewritten every launch
+          addHNAccount(account);
+        });
+
+      if (primaryUserIdKeys.length == 0) {
+        print(primaryUserIdKeys);
+        this._primaryAccountId;
+      } else {
+        print(primaryUserIdKeys.first);
+        print(primaryUserIdKeys.first[KEYS_VALUE]);
+        // this._primaryUserId = primaryUserIdKeys.first;
+
+        // final primaryUser = accounts.firstWhere((account) => account[''] == this._primaryAccountId);
+        // this._primaryUserPassword = primaryUser[ACCOUNTS_PASSWORD];
+      }
+    }).then((a) {});
+
+    triggerOnAction(addHNAccount, (HNAccount user) async {
+      _accounts[user.id] = user;
+
+      print('Adding ${user.id} to SQLite');
+
+      await this._accountsDb.rawInsert(
+        '''
+        INSERT OR REPLACE INTO $ACCOUNTS_TABLE ($ACCOUNTS_ID, $ACCOUNTS_EMAIL, $ACCOUNTS_PASSWORD, $ACCOUNTS_ACCESS_TOKEN)
+          VALUES (?, ?, ?, ?);
+        ''',
+        [user.id, user.email, user.password, user.accessToken]
+      );
+
+      print('Added ${user.id} to SQLite');
+
+      setPrimaryHNAccount(user.id);
+    });
+
+    triggerOnAction(removeHNAccount, (String userId) async {
+      _accounts.remove(userId);
+
+      print('Removing $userId from SQLite');
+
+      await this._accountsDb.delete(
+        ACCOUNTS_TABLE,
+        where: '$ACCOUNTS_ID = ?',
+        whereArgs: [userId],
+      );
+
+      print('Removed $userId from SQLite');
+
+      if (this._accounts.length == 0) {
+        await this._keysDb.delete(
+          KEYS_TABLE,
+          where: '$KEYS_ID = ?',
+          whereArgs: [KEY_PRIMARY_ACCOUNT_ID],
+        );
+      } else {
+        final newPrimaryUserId = this._accounts.values.first.id;
+        setPrimaryHNAccount(newPrimaryUserId);
+      }
+    });
+
+    triggerOnAction(setPrimaryHNAccount, (String userId) async {
+      this._primaryAccountId = userId;
+
+      await this._keysDb.rawInsert(
+        '''
+        INSERT OR REPLACE INTO $KEYS_TABLE ($KEYS_ID, $KEYS_VALUE)
+          VALUES (?, ?);
+        ''',
+        [KEY_PRIMARY_ACCOUNT_ID, userId],
+      );
+    });
+  }
+
+  String get primaryAccountId => this._primaryAccountId;
+  HNAccount get primaryAccount => this._accounts[this._primaryAccountId];
+  Map<String, HNAccount> get accounts => new Map.unmodifiable(this._accounts);
+}
+
+final StoreToken accountStoreToken = new StoreToken(new HNAccountStore());
