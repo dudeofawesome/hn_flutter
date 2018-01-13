a1626339de2e638c569c282e12e241a4ac5fcd0d
Louis Orleans <louis@orleans.io>
2018-01-12 20:45:00 -0800

    🔨 use HNItemService's getItemById

--- lib/sdk/hn_comment_service.dart
+++ lib/sdk/hn_comment_service.dart
@@ -3,31 +3,22 @@ import 'package:http/http.dart' as http;
 import 'dart:convert' show JSON;
 
 import 'package:hn_flutter/sdk/hn_config.dart';
+import 'package:hn_flutter/sdk/hn_item_service.dart';
 import 'package:hn_flutter/sdk/models/hn_item.dart';
 import 'package:hn_flutter/sdk/actions/hn_item_actions.dart';
 
 class HNCommentService {
   HNConfig _config = new HNConfig();
+  HNItemService _itemService = new HNItemService();
 
   getChildComments (HNItem item) async {
     item.kids.forEach((child) => http.get('${this._config.url}/item/$child')
       .then((res) => JSON.decode(res.body))
       .then((List<int> body) => body.sublist(0, 5))
-      .then((List<int> body) => Future.wait(body.map((itemId) => this.getItemByID(itemId)).toList()))
+      .then((List<int> body) => Future.wait(body.map((itemId) => this._itemService.getItemByID(itemId)).toList()))
       .then((List<HNItem> children) {
         children.forEach((child) => addHNItem(new HNItemAction(child)));
       })
     );
-  }
-
-  Future<HNItem> getItemByID (int id) {
-    addHNItem(new HNItemAction(new HNItem(id: id), new HNItemStatus(loading: true)));
-
-    return http.get('${this._config.url}/item/$id.json')
-      .then((res) => JSON.decode(res.body))
-      .then((item) => new HNItem.fromMap(item))
-      .then((item) {
-        addHNItem(new HNItemAction(item));
-      });
   }
 }
