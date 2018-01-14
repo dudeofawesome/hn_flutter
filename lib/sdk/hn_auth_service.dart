dbfe0b47651316a66244eacf6061e7651d758ff2
Louis Orleans <louis@orleans.io>
2018-01-13 23:58:12 -0800

    🚧🔒✨ log in and store access token

--- lib/sdk/hn_auth_service.dart
+++ lib/sdk/hn_auth_service.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+import 'dart:convert' show UTF8, JSON;
+import 'dart:io' show HttpClient, HttpStatus, ContentType;
 
 import 'package:http/http.dart' as http;
 
@@ -9,6 +11,8 @@ import 'package:hn_flutter/sdk/models/hn_account.dart';
 class HNAuthService {
   static final HNAuthService _singleton = new HNAuthService._internal();
 
+  final _httpClient = new HttpClient();
+
   HNConfig _config = new HNConfig();
 
   HNAuthService._internal ();
@@ -18,25 +22,32 @@ class HNAuthService {
   }
 
   Future<bool> addAccount (String userId, String userPassword) async {
-    return http.post(
-        '${this._config.apiHost}/login',
-        body: {
-          'acct': userId,
-          'pw': userPassword,
-        },
-      )
+    final req = await _httpClient.postUrl(Uri.parse('${this._config.apiHost}/login'))
+      ..headers.contentType = new ContentType('application', 'x-www-form-urlencoded', charset: 'utf-8')
+      ..write('acct=$userId&pw=$userPassword');
+
+    return req.close()
       .then((res) {
-        print(res);
-        print(res.body);
-        if (!res.body.contains('Bad login.')) {
-          addHNAccount(new HNAccount(
-            id: userId,
-            password: userPassword,
-          ));
-          return true;
+        if (
+          (res.statusCode == HttpStatus.OK || res.statusCode == HttpStatus.MOVED_TEMPORARILY) &&
+          res.cookies.firstWhere((cookie) => cookie.name == 'user') != null
+        ) {
+          return res;
         } else {
-          return false;
+          throw res;
         }
+      })
+      .then((res) async {
+        addHNAccount(new HNAccount(
+          id: userId,
+          password: userPassword,
+          accessToken: res.cookies.firstWhere((cookie) => cookie.name == 'user').value.split('&')[1],
+        ));
+        return true;
+      })
+      .catchError((err) {
+        print(err);
+        return false;
       });
   }
 
