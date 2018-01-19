b33cc71e64ea535612e7ac4f2cac882743f45bb2
Louis Orleans <louis@orleans.io>
2018-01-18 17:47:17 -0800

    ✨ save story sort mode to SQLite

--- lib/sdk/sqflite_vals.dart
+++ lib/sdk/sqflite_vals.dart
@@ -10,3 +10,4 @@ const KEYS_TABLE = 'keys';
 const KEYS_ID = 'id';
 const KEYS_VALUE = 'value';
 const KEY_PRIMARY_ACCOUNT_ID = 'primary_account_id';
+const KEY_STORY_SORT_MODE = 'story_sort_mode';
--- lib/sdk/stores/ui_store.dart
+++ lib/sdk/stores/ui_store.dart
@@ -1,9 +1,44 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter_flux/flutter_flux.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'package:hn_flutter/sdk/actions/ui_actions.dart';
+import 'package:hn_flutter/sdk/sqflite_vals.dart';
 
 class UIStore extends Store {
-  UIStore () {
+  static final UIStore _singleton = new UIStore._internal();
+
+  Directory _documentsDirectory;
+  Database _keysDb;
+
+  UIStore._internal () {
+    new Future(() async {
+      this._documentsDirectory = await getApplicationDocumentsDirectory();
+
+      String keysPath = join(this._documentsDirectory.path, KEYS_DB);
+      this._keysDb = await openDatabase(keysPath, version: 1, onCreate: (Database db, int version) async {
+        print('CREATING KEYS TABLE');
+        await db.execute('CREATE TABLE $KEYS_TABLE ($KEYS_ID TEXT PRIMARY KEY, $KEYS_VALUE TEXT)');
+      });
+
+      final storySortModeKeys = await this._keysDb.query(
+        KEYS_TABLE,
+        columns: [KEYS_ID, KEYS_VALUE],
+        where: '$KEYS_ID = ?',
+        whereArgs: [KEY_STORY_SORT_MODE],
+        limit: 1,
+      );
+
+      if (storySortModeKeys.length > 0) {
+        print('primary account was ${storySortModeKeys.first[KEYS_VALUE]}');
+        setStorySortMode(SortModes.values[int.parse(storySortModeKeys.first[KEYS_VALUE])]);
+      }
+    }).then((a) {});
+
     triggerOnAction(selectItem, (int selection) {
       if (this._selectedItem != selection) {
         this._selectedItem = selection;
@@ -12,10 +47,20 @@ class UIStore extends Store {
       }
     });
 
-    triggerOnAction(setStorySortMode, (SortModes sortMode) {
+    triggerOnAction(setStorySortMode, (SortModes sortMode) async {
       _sortMode = sortMode;
+
+      await this._keysDb.rawInsert(
+        '''
+        INSERT OR REPLACE INTO $KEYS_TABLE ($KEYS_ID, $KEYS_VALUE)
+          VALUES (?, ?);
+        ''',
+        [KEY_STORY_SORT_MODE, _sortMode.index]
+      );
     });
   }
+
+  factory UIStore () => _singleton;
 
   int _selectedItem;
   SortModes _sortMode = SortModes.TOP;
