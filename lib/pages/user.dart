d5c54c41d75ab169838836f6c8ce7e987b546789
Louis Orleans <louis@orleans.io>
2018-01-31 17:10:47 -0800

    🔥 remove unused code

--- lib/pages/user.dart
+++ lib/pages/user.dart
@@ -121,27 +121,3 @@ const List<Choice> choices = const <Choice>[
   const Choice(title: 'Submitted', icon: Icons.forum),
   const Choice(title: 'Comments', icon: Icons.chat),
 ];
-
-class ChoiceCard extends StatelessWidget {
-  const ChoiceCard({ Key key, this.choice }) : super(key: key);
-
-  final Choice choice;
-
-  @override
-  Widget build(BuildContext context) {
-    final TextStyle textStyle = Theme.of(context).textTheme.display1;
-    return new Card(
-      color: Colors.white,
-      child: new Center(
-        child: new Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            new Icon(choice.icon, size: 128.0, color: textStyle.color),
-            new Text(choice.title, style: textStyle),
-          ],
-        ),
-      ),
-    );
-  }
-}
