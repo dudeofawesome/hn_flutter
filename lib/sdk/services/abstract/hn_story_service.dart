786c6fe59b45dd92a0aa48ddc4ea7c225958ad8f
Louis Orleans <louis@orleans.io>
2018-02-07 19:04:11 -0800

    🚧🚚 move HNStoryService

---
+++ lib/sdk/services/abstract/hn_story_service.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+import 'dart:io' show Cookie;
+
+abstract class HNStoryService {
+  Future<List<int>> getTopStories ({
+    int skip = 0,
+    Cookie accessCookie,
+  });
+
+  Future<List<int>> getNewStories ({
+    int skip = 0,
+    Cookie accessCookie,
+  });
+
+  Future<List<int>> getBestStories ({
+    int skip = 0,
+    Cookie accessCookie,
+  });
+
+  Future<List<int>> getAskStories ({
+    int skip = 0,
+    Cookie accessCookie,
+  });
+
+  Future<List<int>> getShowStories ({
+    int skip = 0,
+    Cookie accessCookie,
+  });
+
+  Future<List<int>> getJobStories ({
+    int skip = 0,
+    Cookie accessCookie,
+  });
+}
---
+++ lib/sdk/services/prod/hn_story_service.dart
@@ -0,0 +1,66 @@
+import 'dart:async';
+import 'dart:io' show Cookie;
+import 'package:http/http.dart' as http;
+import 'dart:convert' show JSON;
+
+import 'package:hn_flutter/sdk/services/abstract/hn_story_service.dart';
+import 'package:hn_flutter/sdk/hn_config.dart';
+import 'package:hn_flutter/sdk/models/hn_item.dart';
+import 'package:hn_flutter/sdk/actions/hn_item_actions.dart';
+import 'package:hn_flutter/sdk/services/hn_item_service.dart';
+
+class HNStoryServiceProd implements HNStoryService {
+  HNConfig _config = new HNConfig();
+  HNItemService _hnItemService = new HNItemService();
+
+  Future<List<int>> _getStories (
+    String sort,
+    {
+      int skip = 0,
+      Cookie accessCookie,
+    }
+  ) {
+    return http.get('${this._config.url}/$sort.json')
+      .then((res) => JSON.decode(res.body))
+      .then((List<int> itemIds) {
+        setStorySort(itemIds);
+        return itemIds;
+      });
+      // .then((List<int> itemIds) => [itemIds, itemIds.sublist(skip, skip + 10)])
+      // .then((List<List<int>> body) =>
+      //   [body[0], Future.wait(body[1].map((itemId) => _hnItemService.getItemByID(itemId, accessCookie)).toList())])
+      // .then((stories) {
+      //   setStorySort(stories[0]);
+      // });
+  }
+
+  Future<List<int>> getTopStories ({
+    int skip = 0,
+    Cookie accessCookie,
+  }) => this._getStories('topstories', skip: skip, accessCookie: accessCookie);
+
+  Future<List<int>> getNewStories ({
+    int skip = 0,
+    Cookie accessCookie,
+  }) => this._getStories('newstories', skip: skip, accessCookie: accessCookie);
+
+  Future<List<int>> getBestStories ({
+    int skip = 0,
+    Cookie accessCookie,
+  }) => this._getStories('beststories', skip: skip, accessCookie: accessCookie);
+
+  Future<List<int>> getAskStories ({
+    int skip = 0,
+    Cookie accessCookie,
+  }) => this._getStories('askstories', skip: skip, accessCookie: accessCookie);
+
+  Future<List<int>> getShowStories ({
+    int skip = 0,
+    Cookie accessCookie,
+  }) => this._getStories('showstories', skip: skip, accessCookie: accessCookie);
+
+  Future<List<int>> getJobStories ({
+    int skip = 0,
+    Cookie accessCookie,
+  }) => this._getStories('jobstories', skip: skip, accessCookie: accessCookie);
+}
