1a0166c3df37a6e66231b9d44b89816851409fce
Louis Orleans <louis@orleans.io>
2018-02-09 23:17:58 -0800

    🚧✨⚡️ initial work on using isolates for API

--- lib/sdk/services/prod/hn_user_service.dart
+++ lib/sdk/services/prod/hn_user_service.dart
@@ -1,6 +1,9 @@
 import 'dart:async';
+import 'dart:isolate';
 import 'package:http/http.dart' as http;
 import 'dart:convert' show JSON;
+
+import 'package:flutter/foundation.dart';
 
 import 'package:hn_flutter/sdk/services/abstract/hn_user_service.dart';
 import 'package:hn_flutter/sdk/hn_config.dart';
@@ -8,16 +11,66 @@ import 'package:hn_flutter/sdk/models/hn_user.dart';
 import 'package:hn_flutter/sdk/actions/hn_user_actions.dart';
 
 class HNUserServiceProd implements HNUserService {
-  HNConfig _config = new HNConfig();
+  final _config = new HNConfig();
+  final _receivePort = new ReceivePort();
+  SendPort _sendPort;
 
-  Future<HNUser> getUserByID (String id) {
+  HNUserServiceProd () {
+    this._init();
+  }
+
+  Future<Null> _init () async {
+    await Isolate.spawn(this._onMessage, this._receivePort.sendPort);
+    this._sendPort = await _receivePort.first;
+  }
+
+  Future<Null> _onMessage (SendPort sendPort) async {
+    final port = new ReceivePort();
+    sendPort.send(port.sendPort);
+
+    // handle message passing
+    await for (final msg in port) {
+      final _IsolateMessage data = msg[0];
+      final SendPort replyTo = msg[1];
+
+      switch (data.type) {
+        case _IsolateMessageType.GET_USER_BY_ID:
+          final user = await http.get('${this._config.url}/user/${data.data}.json')
+            .then((res) => JSON.decode(res.body))
+            .then((user) => new HNUser.fromMap(user));
+          replyTo.send(user);
+          break;
+      }
+
+      // if (data == 'bar') port.close();
+    }
+  }
+
+  Future<HNUser> getUserByID (String id) async {
     addHNUser(new HNUser(id: id, computed: new HNUserComputed(loading: true)));
 
-    return http.get('${this._config.url}/user/$id.json')
-      .then((res) => JSON.decode(res.body))
-      .then((user) => new HNUser.fromMap(user))
-      .then((user) {
-        addHNUser(user);
-      });
+    final response = new ReceivePort();
+    this._sendPort.send([new _IsolateMessage(
+      type: _IsolateMessageType.GET_USER_BY_ID,
+      data: id,
+    ), response.sendPort]);
+
+    final user = await response.first;
+    addHNUser(user);
+    return user;
   }
+}
+
+class _IsolateMessage {
+  _IsolateMessageType type;
+  dynamic data;
+
+  _IsolateMessage ({
+    @required this.type,
+    @required this.data,
+  });
+}
+
+enum _IsolateMessageType {
+  GET_USER_BY_ID,
 }
