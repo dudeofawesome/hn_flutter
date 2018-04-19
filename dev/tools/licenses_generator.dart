37553c7c7e6fd3c113195c392237f83b2001d418
Louis Orleans <louis@orleans.io>
2018-04-19 16:04:50 -0700

    ✨ improve utility of license_generator

--- dev/tools/licenses_generator.dart
+++ dev/tools/licenses_generator.dart
@@ -2,17 +2,85 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:ansicolor/ansicolor.dart';
+import 'package:args/command_runner.dart';
 import 'package:path/path.dart' as path;
 import 'package:pub_cache/pub_cache.dart';
 import 'package:yaml/yaml.dart';
 
-void main() async {
-  final licenses = await generateLicenses();
-  print('Found licenses for ${licenses.keys.join(', ')}');
-  final licensesJson = json.encode(licenses);
-  final licensesFile =
-      new File(path.join(Directory.current.path, 'licenses.json'));
-  await licensesFile.writeAsString(licensesJson);
+import 'package:hn_flutter/utils/dedent.dart';
+
+void main(List<String> args) async {
+  final runner = new CommandRunner(
+      'licenses_generator', 'Generate licenses file from dependencies')
+    ..addCommand(new GenerateCommand());
+
+  try {
+    await runner.run(args);
+    exit(0);
+  } catch (err) {
+    final pen = new AnsiPen()..red();
+    print(pen(err.toString()));
+    exit(1);
+  }
+}
+
+class GenerateCommand extends Command {
+  get name => 'generate';
+  List<String> get aliases => const ['gen', 'g'];
+  get description => 'Generate licenses file from dependencies';
+
+  GenerateCommand() {
+    this.argParser
+      ..addOption(
+        'output',
+        abbr: 'o',
+        defaultsTo: 'assets/strings/licenses.json',
+        help: 'Path to write license json to',
+      )
+      ..addFlag(
+        'transitive',
+        negatable: true,
+        defaultsTo: false,
+        help: dedent('''
+          Add licenses for transitive dependencies too
+          (Currently not supported)
+        '''),
+      )
+      ..addMultiOption(
+        'dependency',
+        abbr: 'd',
+        help:
+            '<name:license-path> A dependency name and license file path. Multiple allowed.',
+      );
+  }
+
+  Future<Null> run() async {
+    if (argResults['output'] == null) throw 'output is required';
+
+    // find licenses from pubspec
+    final pubLicenses = await generateLicenses();
+    print(
+        'Found ${pubLicenses.keys.length} licenses from pubspec for ${pubLicenses.keys.join(', ')}');
+
+    // add manually specified licenses
+    final manualLicenses = new Map<String, String>();
+    for (final String dep in argResults['dependency']) {
+      final String name = dep.split(':')[0];
+      final String path = dep.split(':')[1];
+      final licenseFile = new File(path);
+      if (!await licenseFile.exists()) throw 'License $path not found';
+      manualLicenses[name] = await licenseFile.readAsString();
+    }
+    print(
+        'Found ${manualLicenses.keys.length} licenses from pubspec for ${manualLicenses.keys.join(', ')}');
+
+    // write licenses to json file
+    final licensesJson = json.encode(
+        new Map<String, String>()..addAll(pubLicenses)..addAll(manualLicenses));
+    final licensesFile = new File(argResults['output']);
+    await licensesFile.writeAsString(licensesJson);
+  }
 }
 
 Future<Map<String, String>> generateLicenses() async {
