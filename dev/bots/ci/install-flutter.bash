8c659e1ca0c7bf841edd5e3b6f0836b643fe1685
Louis Orleans <louis@orleans.io>
2018-04-25 20:54:21 -0700

    👷🐛 fix script path

--- dev/bots/ci/install-flutter.bash
+++ dev/bots/ci/install-flutter.bash
@@ -6,7 +6,7 @@ git clone https://github.com/flutter/flutter.git -b beta
 export FLUTTER_HOME=`pwd`/flutter
 export PATH=`pwd`/flutter/bin:$PATH
 pushd flutter
-FLUTTER_VERSION=$(dart get-flutter-version.dart)
+FLUTTER_VERSION=$(dart dev/bots/ci/get-flutter-version.dart)
 git checkout $FLUTTER_VERSION
 popd
 flutter doctor
